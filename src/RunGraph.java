7963dde2c32157a241973c1293105af1932bd214
EvianMcKeown <evian.mck@gmail.com>
2023-05-06 13:32:40 +0200

    -runGraph initial commit WIP

---
+++ src/RunGraph.java
@@ -0,0 +1,16 @@
+import java.io.File;
+
+public class RunGraph {
+    public static void main(String[] args) {
+
+        int[] vArray = { 10, 20, 30, 40, 50 };
+        int[] eArray = { 20, 35, 50, 65, 80 };
+
+        try {
+            File createFile = new File("");
+        } catch (Exception e) {
+
+        }
+
+    }
+}
